c63f7b39198a717e77c34c8d16111477071c4afb
Mykola Beznis <beznosn@gmail.com>
2022-12-02 19:12:13 +0200

    Refactor String extension

--- day2.playground/Pages/playground.xcplaygroundpage/Contents.swift
+++ day2.playground/Pages/playground.xcplaygroundpage/Contents.swift
@@ -2,17 +2,12 @@ import UIKit
 import Foundation
 
 extension String {
-
-    var length: Int {
-        return count
-    }
-
     subscript (i: Int) -> String {
         return self[i ..< i + 1]
     }
 
     func substring(fromIndex: Int) -> String {
-        return self[min(fromIndex, length) ..< length]
+        return self[min(fromIndex, count) ..< count]
     }
 
     func substring(toIndex: Int) -> String {
@@ -20,8 +15,8 @@ extension String {
     }
 
     subscript (r: Range<Int>) -> String {
-        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
-                                            upper: min(length, max(0, r.upperBound))))
+        let range = Range(uncheckedBounds: (lower: max(0, min(count, r.lowerBound)),
+                                            upper: min(count, max(0, r.upperBound))))
         let start = index(startIndex, offsetBy: range.lowerBound)
         let end = index(start, offsetBy: range.upperBound - range.lowerBound)
         return String(self[start ..< end])
