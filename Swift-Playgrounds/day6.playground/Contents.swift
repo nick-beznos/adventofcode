e631b5fa3fc9a2b01e4549314e8dd6e8322911a5
Mykola Beznis <beznosn@gmail.com>
2022-12-06 13:12:23 +0200

    Add day 6

---
+++ Swift-Playgrounds/day6.playground/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+import RegexBuilder
+
+let fileURL = Bundle.main.url(forResource: "Input", withExtension: nil)
+let input = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+
+let array = Array(input)
+
+func findStartIndexWithRange(_ initialStartIndex: Int, _ initialEndIndex: Int) -> Int {
+    var startIndex = initialStartIndex
+    var endIndex = initialEndIndex
+
+    var startMarker = 0
+
+    var keepSearching = true
+
+    while keepSearching {
+        guard array.count > endIndex + 1 else {
+            keepSearching = false
+            break
+        }
+
+        let checkedValues = array[startIndex...endIndex]
+        let uniqueValues = Array(Set(checkedValues))
+
+        if checkedValues.count == uniqueValues.count {
+            keepSearching = false
+            startMarker = endIndex + 1
+        }
+
+        startIndex += 1
+        endIndex += 1
+    }
+
+    return startMarker
+}
+
+print(findStartIndexWithRange(0, 3))
+print(findStartIndexWithRange(0, 13))
